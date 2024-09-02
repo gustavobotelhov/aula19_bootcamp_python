4c92804a3c53300c869540780237bb70d1cc2d6d
Gustavo Botelho Vieira <gu-botelho@hotmail.com>
2024-09-01 22:15:24 -0300

    adicionando docker file

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException, Depends
 from sqlalchemy.orm import Session
 import models
 import database
-from typing import List, Union
+from typing import List
 from schema import Item, ItemBase, ItemCreate
 
 app = FastAPI()
